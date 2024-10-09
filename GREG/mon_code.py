c55a9b6d6b746d4ac8abf33f8cce92a4628ea5a5
GregoireLorea <gregoire.lorea@gmail.com>
2024-10-09 19:28:01 +0200

    annotation

--- GREG/mon_code.py
+++ GREG/mon_code.py
@@ -4,16 +4,21 @@ questions_melange =  #-> LISTE PRNG QUESTION CHARLES
 print("Le questionnaire est une liste de questions.")
 
 
-reponse_player : []
+
+
+
+
+liste_reponse : []
+liste_reponse_player : []
 for q in range(len(questions_melange)):
-    print("\tQuestion " + str(q+1) + ": \"" + questions_melange[q][0] + "\"")
+    print("\tQuestion " + str(q+1) + ": \"" + questions_melange[q][0] + "\"")                   #Print la question
 
     for r in range(len(questions_melange[q][1])):
-        print("\t\tReponses " + str(r+1) + ":" + questions_melange[q][1][r][0] + "\"")
+        print("\t\tReponses " + str(r+1) + ":" + questions_melange[q][1][r][0] + "\"")           #Print les réponses
 
     while True:
         reponse = input(str("Votre réponse (en chiffre(1, 2, 3,...)) ? "))
-        if reponse.isdigit() and reponse <= str(r) and reponse > str(0):
+        if reponse.isdigit() and reponse <= str(r) and reponse > str(0):                         #Demande sa réponse
             break
         else:
             print("Votre format de réponse est incorrect. Veuillez répondre à la question avec un chiffre correspondant à la réponse.")
@@ -21,7 +26,7 @@ for q in range(len(questions_melange)):
     
 
     if questions_melange[q][1][reponse-1][1] == True:
-        reponse_player.append([1])             #Le joueur à juste, on ajoute 1 à la liste reponse player
+        liste_reponse_player.append([1])             #Le joueur à juste, on ajoute 1 à la liste reponse player
 
     if questions_melange[q][1][reponse-1][1] == False:
-        reponse_player.append([0])             #Le joueur a faux, on ajoute 0 à la liste reponse player
+        liste_reponse_player.append([0])             #Le joueur a faux, on ajoute 0 à la liste reponse player
