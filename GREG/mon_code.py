34dc42548de9009fec23e2ad40daf22576924d9e
GregoireLorea <gregoire.lorea@gmail.com>
2024-10-09 19:20:41 +0200

    modif et fix d'un bug

--- GREG/mon_code.py
+++ GREG/mon_code.py
@@ -11,15 +11,17 @@ for q in range(len(questions_melange)):
     for r in range(len(questions_melange[q][1])):
         print("\t\tReponses " + str(r+1) + ":" + questions_melange[q][1][r][0] + "\"")
 
-
     while True:
         reponse = input(str("Votre réponse (en chiffre(1, 2, 3,...)) ? "))
-        if reponse.isdigit() and 0 <= reponse >= r:
+        if reponse.isdigit() and reponse <= str(r) and reponse > str(0):
             break
         else:
             print("Votre format de réponse est incorrect. Veuillez répondre à la question avec un chiffre correspondant à la réponse.")
 
-    if questions_melange[q][1][r][1] == True:
-        reponse_player.append([1])
-    if questions_melange[q][1][r][1] == False:
-        reponse_player.append([0]) 
+    
+
+    if questions_melange[q][1][reponse-1][1] == True:
+        reponse_player.append([1])             #Le joueur à juste, on ajoute 1 à la liste reponse player
+
+    if questions_melange[q][1][reponse-1][1] == False:
+        reponse_player.append([0])             #Le joueur a faux, on ajoute 0 à la liste reponse player
