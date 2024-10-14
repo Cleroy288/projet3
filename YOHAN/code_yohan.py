253b63c254c06e416a7ed46337a8898701b00699
yohan <yohan@wifi-student15-650.sri.ucl.ac.be>
2024-10-14 12:04:49 +0200

    commentaires

--- YOHAN/code_yohan.py
+++ YOHAN/code_yohan.py
@@ -1,4 +1,17 @@
 import random
 
 def functionYohan:
+	#prend en entrée 2 liste la liste des réponses du joueur ainsi que la liste contenant toute les questions et réponses 
+
+	# question par question , compare la liste des réponses du joueur avec la liste des vrai réponses 
+    # si réponse n joueur = réponse n list -->> réponse True
+    # si réponse n joueur != réponse n list -->> réponse False
+
+	# crée une liste qui contient n éléments en fonction du nombre de questions 
+	# --> pour chaque bonne réponse du joueur dans la lsite elle emt VRAI à l'index de la question 
+	# --> si le joueur a faux elle marque "FAUX" à l'index de la question 
+
+	# elle renvoi la liste qu'elle a crée 
+	# return liste avce VRAI et FAUX
+    
     print("Mon taff")
